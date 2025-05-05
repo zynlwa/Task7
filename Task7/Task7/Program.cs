62689a08bff2240668911ef3daffd1bc77d483a9
Shefa <zeyna@example.com>
2025-05-05 14:09:29 +0400

    first commit

---
+++ Task7/Task7/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace Task7
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Pow(3);
+        //}
+        //public static void Pow(int a)
+        //{
+        //    int result = a* a;
+        //    Console.WriteLine(result);
+        //}
+        //TASK2 
+        //static void Main(string[] args)
+        //{
+        //    int[] result =PowArr(1,2,3,4,5);
+        //    foreach (int i in result)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+
+        //}
+        //public static int[] PowArr(params int[] numbers)
+        //{
+        //    int[] num = new int[numbers.Length];
+
+        //    for (int i = 0; i < numbers.Length; i++) 
+        //    {
+        //        num[i] = numbers[i] * numbers[i];
+
+        //    }
+        //    return num;
+        //Task3
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine(Space("Hikmet Abbasov"));
+        //}
+        //public static string NOSpaces(string word)
+        //{
+        //    string news = "";
+        //    foreach (var i in word)
+        //    {
+        //        if (i != ' ')
+        //        {
+        //            news += i;
+        //        }
+        //    }
+        //    return news;
+        //}
+        //static void Main(string[] args)
+        //{
+        //    int[] number = { 1, 2, 3 };
+        //    number = AdElements(number, 4);
+        //    foreach (int i in number)
+        //    {
+        //        Console.WriteLine(i);
+        //    }
+        //}
+        //public static int[] AdElements(int[] arr, int value)
+        //{
+        //    int[] newArr = new int[arr.Length + 1];
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        newArr[i] = arr[i];  
+        //    }
+        //    newArr[arr.Length] = value;
+        //    return newArr;
+        //}
+        
+    }
+}
+
